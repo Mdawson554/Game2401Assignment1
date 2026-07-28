4b7307803af454c80a8eee8abff6c6537fcb3be3
Mdawson554 <mdawson554@gmail.com>
2026-07-28 13:09:30 -0600

    StoryMarkerResolution

--- Assets/Scripts/Interactions/NPCs.cs
+++ Assets/Scripts/Interactions/NPCs.cs
@@ -5,7 +5,6 @@ using Gameplay;
 using Interactions;
 using Story;
 using UnityEngine;
-
 public class NPCs : MonoBehaviour, IInteractable
 {
     public DialogueSO Dialogue;
@@ -14,79 +13,79 @@ public class NPCs : MonoBehaviour, IInteractable
         
     [Header("Requirements")]
     public List<StoryMarker> RequiredMarkers;
-
     [Header("Unlocks")]
     public List<StoryMarker> MarkersUnlocked;
-
-    private void OnInteracted()
+    
+    private void Start()
     {
-        EventManager.instance.Subscribe<DialogueFinishedEvent>(OnDialogueFinished);
-        if (HasRequirement)
-        {
-            if (RequiredMarkers.Count <=0)
-            {
-                DeliverDialogue();
-            }
-            else
-            {
-                Debug.Log("requirments unfufilled");
-                return;
-            }
-        }
-        DeliverDialogue();
+        EventManager.instance.Subscribe<StoryMarkerUnlockedEvent>(UnlockMarker);
     }
-        
+    
     private void OnDestroy()
     {
         if (EventManager.instance != null)
         {
             EventManager.instance.Unsubscribe<StoryMarkerUnlockedEvent>(UnlockMarker);
+            EventManager.instance.Unsubscribe<DialogueFinishedEvent>(OnDialogueFinished);
         }
     }
-
-    public void OnHoverIn()
+    
+    private void OnInteracted()
     {
-            
+        if (HasRequirement)
+        {
+            if (RequiredMarkers.Count > 0)
+            {
+                Debug.Log($"Requirements unfulfilled for {gameObject.name}. Missing {RequiredMarkers.Count} marker(s)");
+                return;
+            }
+        }
+        EventManager.instance.Subscribe<DialogueFinishedEvent>(OnDialogueFinished);
+        DeliverDialogue();
     }
-
+    
     public void OnInteract()
     {
         OnInteracted();
     }
-
+    
+    public void OnHoverIn()
+    {
+            
+    }
+    
     public void OnHoverOff()
     {
             
     }
-        
-    void UnlockMarker(StoryMarkerUnlockedEvent e)
+
+    private void UnlockMarker(StoryMarkerUnlockedEvent e)
     {
-        EventManager.instance.Unsubscribe<DialogueFinishedEvent>(OnDialogueFinished);
         if (RequiredMarkers.Contains(e.Marker))
         {
-            
-            EventManager.instance.Unsubscribe<StoryMarkerUnlockedEvent>(UnlockMarker);
             RequiredMarkers.Remove(e.Marker);
-        }
-        Debug.Log($"Unlocked Story Marker : {e.Marker.name}");
-    }
-        
-    private void Start()
-    {
-        
-    }
-
-    public void OnDialogueFinished(DialogueFinishedEvent e)
-    {
-        if (EventManager.instance != null)
-        {
-            foreach (var Marker in MarkersUnlocked)
+            Debug.Log($"NPC '{gameObject.name}' now has marker '{e.Marker.name}'. Remaining requirements: {RequiredMarkers.Count}");
+            if (RequiredMarkers.Count == 0)
             {
-                EventManager.instance.Publish(new StoryMarkerUnlockedEvent(Marker));
+                Debug.Log($"NPC '{gameObject.name}' is now unlocked!");
             }
         }
     }
-
+    
+    public void OnDialogueFinished(DialogueFinishedEvent e)
+    {
+        EventManager.instance.Unsubscribe<DialogueFinishedEvent>(OnDialogueFinished);
+        
+        if (EventManager.instance != null)
+        {
+            foreach (var marker in MarkersUnlocked)
+            {
+                EventManager.instance.Publish(new StoryMarkerUnlockedEvent(marker));
+                Debug.Log($"NPC '{gameObject.name}' unlocked marker '{marker.name}'");
+            }
+        }
+    }
+    
     private void DeliverDialogue()
     {
         EventManager.instance.Publish(new StateChangeEvent(GameManager.Instance.playerStateMachine.dialoguestate));
